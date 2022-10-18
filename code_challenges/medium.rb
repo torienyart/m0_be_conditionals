54bee11fc07262d49d38c9bfbcc60f1ed68fbf28
Tori Enyart <torienyart@gmail.com>
2022-10-18 10:06:26 -0600

    Adjust if statement

--- code_challenges/medium.rb
+++ code_challenges/medium.rb
@@ -1,13 +1,11 @@
 good_driving_record = true
-age = 22
+age = 25
 
 if good_driving_record == true && age > 25 
     p "Congrats, you qualify for a discount on your rental!"
 elsif good_driving_record == true || age > 25
     p "You can rent a car!"
-elsif age <= 25
-    p "You will need somebody else to sign for the rental!"
-elsif good_driving_record != true 
+elsif age <= 25 && good_driving_record != true
     p "You will need somebody else to sign for the rental!"
 else
     p "Please contact our customer service department at 1-800-rent"
